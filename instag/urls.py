dff90fc424465c265fad0aa354247fae6b5e3a9d
AokoMercyline <aokomercyline34@gmail.com>
2020-10-18 17:59:18 +0300

    view function for login

--- instag/urls.py
+++ instag/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-
 from django.urls import include, path
 from users import views as user_views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    
+    path('login/',LoginView.as_view(), name='Login'),
     path('', include('instaclone.urls')),
 ]
