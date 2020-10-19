e5ab0bb52b190025a0ea0b0e8a8c6aee542b2567
AokoMercyline <aokomercyline34@gmail.com>
2020-10-19 20:35:13 +0300

    configured email

--- instag/settings.py
+++ instag/settings.py
@@ -142,4 +142,11 @@ LOGIN_URL = 'login'
 
 LOGOUT_REDIRECT_URL = 'instaclone-index'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_USE_TLS =True
+EMAIL_HOST_USER ='ochiengmercyline2020@gmail.com'
+EMAIL_HOST_PASSWORD ='0708434156mercy'
+
 
