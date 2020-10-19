55fe1bd34444c8ef31ca052bf15f39b01e7e18f3
AokoMercyline <aokomercyline34@gmail.com>
2020-10-19 18:22:15 +0300

    url for posting creating and updating and deleting post

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', PostListView.as_view(), name='instaclone-index'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    
+
 ]      
